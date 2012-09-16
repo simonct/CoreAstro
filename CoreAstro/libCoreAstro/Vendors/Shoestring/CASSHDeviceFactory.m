4fde49dcc310563d9dc6d7e71844bef2bd72316c
Simon Taylor <simon@makotechnology.com>
2012-09-16 16:33:03 +0100

    Add Shoestring plugin, no actual device support as yet

---
+++ CoreAstro/libCoreAstro/Vendors/Shoestring/CASSHDeviceFactory.m
@@ -0,0 +1,69 @@
+//
+//  CASSHDeviceFactory.m
+//  CoreAstro
+//
+//  Copyright (c) 2012, Simon Taylor
+// 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy 
+//  of this software and associated documentation files (the "Software"), to deal 
+//  in the Software without restriction, including without limitation the rights 
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+//  copies of the Software, and to permit persons to whom the Software is furnished 
+//  to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in 
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+//  IN THE SOFTWARE.
+//
+
+#import "CASSHDeviceFactory.h"
+
+@interface CASSHDeviceFactory ()
+@end
+
+@implementation CASSHDeviceFactory
+
++ (void)initialize {
+    NSLog(@"CASSHDeviceFactory");
+}
+
++ (SInt32)vendorID {
+    return 0x134A;
+}
+
+- (CASDevice*)createDeviceWithDeviceRef:(void*)dev path:(NSString*)path properties:(NSDictionary*)properties {
+    
+    CASDevice* result = nil;
+    
+    NSNumber* vendorID = [properties objectForKey:@"idVendor"];
+    if ([vendorID isEqualToNumber:[NSNumber numberWithInteger:[[self class] vendorID]]]){
+    
+        NSNumber* productID = [properties objectForKey:@"idProduct"];
+        switch ([productID integerValue]) {
+                
+                // FCUSB[2]
+            case 0x9023:
+            case 0x9024: {
+                NSLog(@"CASSHDeviceFactory: recognised FCUSB[2]");
+            }
+                break;
+                
+                // GPUSB
+            case 0x9020: {
+                NSLog(@"CASSHDeviceFactory: recognised GPUSB");
+            }
+                break;
+        }
+    }
+    
+    return result;
+}
+
+@end
