fa27e710e38481416d1a22640cb4869795475416
Wagner Truppel <trupwl@gmail.com>
2012-09-22 10:25:21 +0100

    Custom segmentation algorithm command line tool now loads an exposure from disk, whose path is passed to it as a command line argument. Test exposure set in the target scheme.

--- CoreAstro Algorithm Sandbox/CoreAstroSegmentation/CoreAstroCustomSegmentationAlg/CoreAstroCustomSegmentationAlg/CASCustomSegmentationAlg.m
+++ CoreAstro Algorithm Sandbox/CoreAstroSegmentation/CoreAstroCustomSegmentationAlg/CoreAstroCustomSegmentationAlg/CASCustomSegmentationAlg.m
@@ -53,6 +53,7 @@
     }
 
     exposure = (CASCCDExposure*) objInDataD;
+    NSLog(@"exposure:\n%@", exposure);
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul), ^{
 
--- CoreAstro Algorithm Sandbox/CoreAstroSegmentation/CoreAstroCustomSegmentationAlg/CoreAstroCustomSegmentationAlg/main.m
+++ CoreAstro Algorithm Sandbox/CoreAstroSegmentation/CoreAstroCustomSegmentationAlg/CoreAstroCustomSegmentationAlg/main.m
@@ -8,30 +8,44 @@
 
 #import <Foundation/Foundation.h>
 
-#import "CASCCDParams.h"
 #import "CASCCDExposure.h"
+#import "CASCCDExposureIO.h"
 
 #import "CASCustomAutoGuider.h"
 
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
 
-        // WLT-QQQ: How do I get an exposure from a pre-existing file?
         // WLT-QQQ: How do I keep the command line tool running until the alg has finished processing the image?
 
-        CASExposeParams params;
-        CASCCDExposure* exposure = [CASCCDExposure exposureWithPixels: nil camera: nil params: params time: nil];
-        
-        CASAutoGuider* guider = [CASCustomAutoGuider autoGuiderWithIdentifier: @"CustomSegmAlg"];
-        [guider locateStars: exposure]; // ignoring the result because the implementation of
-                                        // -locateStars will return nil immediately and run
-                                        // itself in a background thread.
+        if (argc != 2)
+        {
+            NSLog(@"This command line tool requires a single argument, a path to an exposure.");
+            return 0;
+        }
+        NSString* path = [NSString stringWithCString: argv[1] encoding: NSASCIIStringEncoding];
+        NSLog(@"Loading exposure at path:\r'%@'", path);
 
+        CASCCDExposureIO* expIO = [CASCCDExposureIO exposureIOWithPath: path];
+        NSError* error = nil;
+
+        CASCCDExposure* exposure = [[CASCCDExposure alloc] init];
+        BOOL readSuccess = [expIO readExposure: exposure readPixels: YES error: &error];
+
+        if (!readSuccess || error)
+        {
+            NSLog(@"Unable to load exposure at path:\n'%@'", path);
+        }
+        else
+        {
+            CASAutoGuider* guider = [CASCustomAutoGuider autoGuiderWithIdentifier: @"CustomSegmAlg"];
+            [guider locateStars: exposure]; // ignoring the result because the implementation of
+                                            // -locateStars will return nil immediately and run
+                                            // itself in a background thread.
+        }
     }
 
     return 0;
 }
-
