13a7bf2512a91bf9e2ac355b4c9f1a4a04d96245
Simon Taylor <simon@makotechnology.com>
2012-09-23 08:03:31 +0100

    Nil out the transport on disconnect to ensure the interface is closed

--- CoreAstro/libCoreAstro/Vendors/SXCCD/SXCCDDevice.m
+++ CoreAstro/libCoreAstro/Vendors/SXCCD/SXCCDDevice.m
@@ -58,7 +58,10 @@
 
 - (void)disconnect
 {
+    // todo; this mainly belongs in a base class
     self.connected = NO;
+    // todo: this isn't supporting reconnect
+    self.transport = nil;
     [NSObject cancelPreviousPerformRequestsWithTarget:self];
 }
 
