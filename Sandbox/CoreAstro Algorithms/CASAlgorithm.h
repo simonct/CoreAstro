b9102e1c0c40a95c8adb2764cfda847269d3ad03
Wagner Truppel <trupwl@gmail.com>
2012-10-17 01:19:52 +0100

    CoreAstroAlgs: added properties to CASAlgorithm for subclasses to use if needed. Declared a type for the algorithm completion block.

--- Sandbox/CoreAstro Algorithms/CASAlgorithm.h
+++ Sandbox/CoreAstro Algorithms/CASAlgorithm.h
@@ -3,28 +3,36 @@
 //  CoreAstro
 //
 //  Copyright (c) 2012, Wagner Truppel
-// 
-//  Permission is hereby granted, free of charge, to any person obtaining a copy 
-//  of this software and associated documentation files (the "Software"), to deal 
-//  in the Software without restriction, including without limitation the rights 
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-//  copies of the Software, and to permit persons to whom the Software is furnished 
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is furnished
 //  to do so, subject to the following conditions:
 //
-//  The above copyright notice and this permission notice shall be included in 
+//  The above copyright notice and this permission notice shall be included in
 //  all copies or substantial portions of the Software.
 //
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 //  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 //  IN THE SOFTWARE.
 //
 
 
+typedef void(^CASAlgorithmCompletionBlock)(NSDictionary*);
+
+
 @interface CASAlgorithm: NSObject
+
+@property (readonly, nonatomic, strong) NSDictionary* dataD;
+@property (readonly, nonatomic) dispatch_queue_t completionQueue;
+@property (readonly, nonatomic, strong) CASAlgorithmCompletionBlock completionBlock;
+
 
 // A completely general interface for algorithms.
 //
@@ -39,7 +47,7 @@
 - (void) executeWithDictionary: (NSDictionary*) dataD
                completionAsync: (BOOL) async
                completionQueue: (dispatch_queue_t) queue
-               completionBlock: (void(^)(NSDictionary*)) block;
+               completionBlock: (CASAlgorithmCompletionBlock) block;
 
 
 // For subclass use only.
@@ -50,7 +58,7 @@
 
 // Utility method.
 // For subclass use only and not to be overridden.
-- (void) dispatchBlock: (void(^)(NSDictionary*)) block
+- (void) dispatchBlock: (CASAlgorithmCompletionBlock) block
                toQueue: (dispatch_queue_t) queue
                  async: (BOOL) async
           withArgument: (NSDictionary*) resultsD;
--- Sandbox/CoreAstro Algorithms/CASAlgorithm.m
+++ Sandbox/CoreAstro Algorithms/CASAlgorithm.m
@@ -3,21 +3,21 @@
 //  CoreAstro
 //
 //  Copyright (c) 2012, Wagner Truppel
-// 
-//  Permission is hereby granted, free of charge, to any person obtaining a copy 
-//  of this software and associated documentation files (the "Software"), to deal 
-//  in the Software without restriction, including without limitation the rights 
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-//  copies of the Software, and to permit persons to whom the Software is furnished 
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is furnished
 //  to do so, subject to the following conditions:
 //
-//  The above copyright notice and this permission notice shall be included in 
+//  The above copyright notice and this permission notice shall be included in
 //  all copies or substantial portions of the Software.
 //
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 //  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 //  IN THE SOFTWARE.
@@ -26,17 +26,30 @@
 
 #import "CASAlgorithm.h"
 
-@implementation CASAlgorithm
 
+@interface CASAlgorithm ()
+
+@property (readwrite, nonatomic, strong) NSDictionary* dataD;
+@property (readwrite, nonatomic) dispatch_queue_t completionQueue;
+@property (readwrite, nonatomic, strong) CASAlgorithmCompletionBlock completionBlock;
+
+@end
+
+
+@implementation CASAlgorithm
 
 // For client use only. Not to be overridden by subclasses.
 - (void) executeWithDictionary: (NSDictionary*) dataD
                completionAsync: (BOOL) async
                completionQueue: (dispatch_queue_t) queue
-               completionBlock: (void(^)(NSDictionary*)) block;
+               completionBlock: (CASAlgorithmCompletionBlock) block;
 {
     // NSLog(@"%s : dataD:\r%@", __FUNCTION__, dataD);
-    
+
+    self.dataD = dataD;
+    self.completionQueue = queue;
+    self.completionBlock = block;
+
     NSDictionary* resultsD = [self resultsFromData: dataD];
 
     [self dispatchBlock: block
@@ -54,13 +67,13 @@
 
 
 // Utility method. Not to be overridden.
-- (void) dispatchBlock: (void(^)(NSDictionary*)) block
+- (void) dispatchBlock: (CASAlgorithmCompletionBlock) block
                toQueue: (dispatch_queue_t) queue
                  async: (BOOL) async
           withArgument: (NSDictionary*) resultsD;
 {
-//    NSLog(@"%s : block == nil? %@ : queue == NULL? %@ : async ? %@ : resultsD:\r%@", __FUNCTION__,
-//          (block ? @"NO" : @"YES"), (queue ? @"NO" : @"YES"), (async ? @"YES" : @"NO"), resultsD);
+    //    NSLog(@"%s : block == nil? %@ : queue == NULL? %@ : async ? %@ : resultsD:\r%@", __FUNCTION__,
+    //          (block ? @"NO" : @"YES"), (queue ? @"NO" : @"YES"), (async ? @"YES" : @"NO"), resultsD);
 
     if (block && queue)
     {
