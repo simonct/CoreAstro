3c887ba332a9a17c1fd0076e721c6d434904424d
Simon Taylor <simon@makotechnology.com>
2012-12-01 11:49:16 +0000

    Output of debayering is now a floating point image

--- CoreAstro/libCoreAstro/Core/CASImageDebayer.mm
+++ CoreAstro/libCoreAstro/Core/CASImageDebayer.mm
@@ -25,6 +25,7 @@
 //  Very basic liner interpolation debayering.
 
 #import "CASImageDebayer.h"
+#import "CASUtilities.h"
 
 @interface CASImageDebayer ()
 - (CGImageRef)debayer:(CASCCDImage*)image;
@@ -42,12 +43,14 @@
 - (CGImageRef)debayer:(CASCCDImage*)image
 {
     CGColorSpaceRef space = CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
-    CGContextRef context = CGBitmapContextCreate(nil, image.size.width, image.size.height, 8, (image.size.width) * 4, space, kCGImageAlphaPremultipliedLast); // RGBA
+    CGContextRef context = CGBitmapContextCreate(nil, image.size.width, image.size.height, 32, (image.size.width) * 4 * 4, space, kCGImageAlphaPremultipliedLast|kCGBitmapFloatComponents|kCGBitmapByteOrder32Little);
+    NSAssert(context, [NSString stringWithUTF8String:__PRETTY_FUNCTION__]);
     CFRelease(space);
     
+    typedef struct { float r,g,b,a; } fpixel_t;
+
     float *gp = (float*)[image.floatPixels bytes];
-    
-    uint32_t *cp = (uint32_t*)CGBitmapContextGetData(context);
+    fpixel_t *cp = (fpixel_t*)CGBitmapContextGetData(context);
     bzero(cp, CGBitmapContextGetBytesPerRow(context) * CGBitmapContextGetHeight(context));
     
     const CASSize size = CASSizeMake(image.size.width, image.size.height);
@@ -55,177 +58,182 @@
     #define clip(x,lim) ((x) < 0 ? 0 : (x) >= (lim) ? (lim-1) : (x))
     #define clipx(x) clip(x,size.width)
     #define clipy(y) clip(y,size.height)
-    #define source_pixel(x,y) (*(gp + clipx(x) + clipy(y) * size.width)*255.0)
+    #define source_pixel(x,y) (*(gp + clipx(x) + clipy(y) * size.width))
     #define destination_pixel(x,y) *(cp + clipx(x) + clipy(y) * size.width)
-    #define make_rgba(r,g,b,a) (r) | ((g) << 8) | ((b) << 16) | ((a) << 24);
-    
-    for (int y = 0; y < size.height; y += 2){
+    #define make_rgba(rx,gx,bx,ax) { .r = rx, .g = gx, .b = bx, .a = ax };
         
-        for (int x = 0; x < size.width; x += 2){
+    const NSTimeInterval time = CASTimeBlock(^{
+        
+        for (int y = 0; y < size.height; y += 2){
             
-            const uint8_t a = 0xff;
-            
-            // RG|RG|RG|RG
-            // GB|GB|GB|GB
-            // RG|RG|RG|RG
-            // GB|GB|GB|GB
-            
-            if (self.mode == kCASImageDebayerRGGB){
+            for (int x = 0; x < size.width; x += 2){
                 
-                int i = x, j = y;
+                const float a = 1.0;
                 
-                uint8_t r1 = source_pixel(i,j);
-                uint8_t g1 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
-                uint8_t b1 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
+                // RG|RG|RG|RG
+                // GB|GB|GB|GB
+                // RG|RG|RG|RG
+                // GB|GB|GB|GB
                 
-                destination_pixel(i,j) = make_rgba(r1,g1,b1,a);
+                if (self.mode == kCASImageDebayerRGGB){
+                    
+                    int i = x, j = y;
+                    
+                    float r1 = source_pixel(i,j);
+                    float g1 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
+                    float b1 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
+                    
+                    destination_pixel(i,j) = make_rgba(r1,g1,b1,a);
+                    
+                    i = x + 1, j = y + 1;
+                    
+                    float r2 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
+                    float g2 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
+                    float b2 = source_pixel(i,j);
+                    
+                    destination_pixel(i,j) = make_rgba(r2,g2,b2,a);
+                    
+                    i = x + 1, j = y;
+                    
+                    float r3 = (source_pixel(i,j - 1) + source_pixel(i,j + 1))/2;
+                    float g3 = source_pixel(i,j);
+                    float b3 = (source_pixel(i - 1,j) + source_pixel(i + 1,j))/2;
+                    
+                    destination_pixel(i,j) = make_rgba(r3,g3,b3,a);
+                    
+                    i = x, j = y + 1;
+                    
+                    float r4 = (source_pixel(i - 1,j) + source_pixel(i + 1,j))/2;
+                    float g4 = source_pixel(i,j);
+                    float b4 = (source_pixel(i,j - 1) + source_pixel(i,j + 1))/2;
+                    
+                    destination_pixel(i,j) = make_rgba(r4,g4,b4,a);
+                }
                 
-                i = x + 1, j = y + 1;
+                // GR|GR|GR|GR
+                // BG|BG|BG|BG
+                // GR|GR|GR|GR
+                // BG|BG|BG|BG
                 
-                uint8_t r2 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
-                uint8_t g2 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
-                uint8_t b2 = source_pixel(i,j);
+                if (self.mode == kCASImageDebayerGRBG){
+                    
+                    int i = x + 1, j = y;
+                    
+                    float r1 = source_pixel(i,j);
+                    float g1 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
+                    float b1 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
+                    
+                    destination_pixel(i,j) = make_rgba(r1,g1,b1,a);
+                    
+                    i = x, j = y + 1;
+                    
+                    float r2 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
+                    float g2 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
+                    float b2 = source_pixel(i,j);
+                    
+                    destination_pixel(i,j) = make_rgba(r2,g2,b2,a);
+                    
+                    i = x, j = y;
+                    
+                    float r3 = (source_pixel(i-1,j) + source_pixel(i+1,j))/2;
+                    float g3 = source_pixel(i,j);
+                    float b3 = (source_pixel(i,j-1) + source_pixel(i,j+1))/2;
+                    
+                    destination_pixel(i,j) = make_rgba(r3,g3,b3,a);
+                    
+                    i = x + 1, j = y + 1;
+                    
+                    float r4 = (source_pixel(i,j-1) + source_pixel(i,j+1))/2;
+                    float g4 = source_pixel(i,j);
+                    float b4 = (source_pixel(i-1,j) + source_pixel(i+1,j))/2;
+                    
+                    destination_pixel(i,j) = make_rgba(r4,g4,b4,a);
+                }
                 
-                destination_pixel(i,j) = make_rgba(r2,g2,b2,a);
+                // BG|BG|BG|BG
+                // GR|GR|GR|GR
+                // BG|BG|BG|BG
+                // GR|GR|GR|GR
                 
-                i = x + 1, j = y;
+                if (self.mode == kCASImageDebayerBGGR){
+                    
+                    int i = x + 1, j = y + 1;
+                    
+                    float r1 = source_pixel(i,j);
+                    float g1 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
+                    float b1 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
+                    
+                    destination_pixel(i,j) = make_rgba(r1,g1,b1,a);
+                    
+                    i = x, j = y;
+                    
+                    float r2 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
+                    float g2 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
+                    float b2 = source_pixel(i,j);
+                    
+                    destination_pixel(i,j) = make_rgba(r2,g2,b2,a);
+                    
+                    i = x + 1, j = y;
+                    
+                    float r3 = (source_pixel(i,j - 1) + source_pixel(i,j + 1))/2;
+                    float g3 = source_pixel(i,j);
+                    float b3 = (source_pixel(i - 1,j) + source_pixel(i + 1,j))/2;
+                    
+                    destination_pixel(i,j) = make_rgba(r3,g3,b3,a);
+                    
+                    i = x, j = y + 1;
+                    
+                    float r4 = (source_pixel(i - 1,j) + source_pixel(i + 1,j))/2;
+                    float g4 = source_pixel(i,j);
+                    float b4 = (source_pixel(i,j - 1) + source_pixel(i,j + 1))/2;
+                    
+                    destination_pixel(i,j) = make_rgba(r4,g4,b4,a);
+                }
                 
-                uint8_t r3 = (source_pixel(i,j - 1) + source_pixel(i,j + 1))/2;
-                uint8_t g3 = source_pixel(i,j);
-                uint8_t b3 = (source_pixel(i - 1,j) + source_pixel(i + 1,j))/2;
+                // GB|GB|GB|GB
+                // RG|RG|RG|RG
+                // GB|GB|GB|GB
+                // RG|RG|RG|RG
                 
-                destination_pixel(i,j) = make_rgba(r3,g3,b3,a);
-                
-                i = x, j = y + 1;
-                
-                uint8_t r4 = (source_pixel(i - 1,j) + source_pixel(i + 1,j))/2;
-                uint8_t g4 = source_pixel(i,j);
-                uint8_t b4 = (source_pixel(i,j - 1) + source_pixel(i,j + 1))/2;
-                
-                destination_pixel(i,j) = make_rgba(r4,g4,b4,a);
-            }
-            
-            // GR|GR|GR|GR
-            // BG|BG|BG|BG
-            // GR|GR|GR|GR
-            // BG|BG|BG|BG
-            
-            if (self.mode == kCASImageDebayerGRBG){
-                
-                int i = x + 1, j = y;
-                
-                uint8_t r1 = source_pixel(i,j);
-                uint8_t g1 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
-                uint8_t b1 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
-                
-                destination_pixel(i,j) = r1 | (g1 << 8) | (b1 << 16) | (a << 24);
-                
-                i = x, j = y + 1;
-                
-                uint8_t r2 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
-                uint8_t g2 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
-                uint8_t b2 = source_pixel(i,j);
-                
-                destination_pixel(i,j) = r2 | (g2 << 8) | (b2 << 16) | (a << 24);
-                
-                i = x, j = y;
-                
-                uint8_t r3 = (source_pixel(i-1,j) + source_pixel(i+1,j))/2;
-                uint8_t g3 = source_pixel(i,j);
-                uint8_t b3 = (source_pixel(i,j-1) + source_pixel(i,j+1))/2;
-                
-                destination_pixel(i,j) = r3 | (g3 << 8) | (b3 << 16) | (a << 24);
-                
-                i = x + 1, j = y + 1;
-                
-                uint8_t r4 = (source_pixel(i,j-1) + source_pixel(i,j+1))/2;
-                uint8_t g4 = source_pixel(i,j);
-                uint8_t b4 = (source_pixel(i-1,j) + source_pixel(i+1,j))/2;
-                
-                destination_pixel(i,j) = r4 | (g4 << 8) | (b4 << 16) | (a << 24);
-            }
-            
-            // BG|BG|BG|BG
-            // GR|GR|GR|GR
-            // BG|BG|BG|BG
-            // GR|GR|GR|GR
-            
-            if (self.mode == kCASImageDebayerBGGR){
-                
-                int i = x + 1, j = y + 1;
-                
-                uint8_t r1 = source_pixel(i,j);
-                uint8_t g1 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
-                uint8_t b1 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
-                
-                destination_pixel(i,j) = r1 | (g1 << 8) | (b1 << 16) | (a << 24);
-                
-                i = x, j = y;
-                
-                uint8_t r2 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
-                uint8_t g2 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
-                uint8_t b2 = source_pixel(i,j);
-                
-                destination_pixel(i,j) = r2 | (g2 << 8) | (b2 << 16) | (a << 24);
-                
-                i = x + 1, j = y;
-                
-                uint8_t r3 = (source_pixel(i,j - 1) + source_pixel(i,j + 1))/2;
-                uint8_t g3 = source_pixel(i,j);
-                uint8_t b3 = (source_pixel(i - 1,j) + source_pixel(i + 1,j))/2;
-                
-                destination_pixel(i,j) = r3 | (g3 << 8) | (b3 << 16) | (a << 24);
-                
-                i = x, j = y + 1;
-                
-                uint8_t r4 = (source_pixel(i - 1,j) + source_pixel(i + 1,j))/2;
-                uint8_t g4 = source_pixel(i,j);
-                uint8_t b4 = (source_pixel(i,j - 1) + source_pixel(i,j + 1))/2;
-                
-                destination_pixel(i,j) = r4 | (g4 << 8) | (b4 << 16) | (a << 24);
-            }
-            
-            // GB|GB|GB|GB
-            // RG|RG|RG|RG
-            // GB|GB|GB|GB
-            // RG|RG|RG|RG
-            
-            if (self.mode == kCASImageDebayerGBRG){
-                
-                int i = x, j = y + 1;
-                
-                uint8_t r1 = source_pixel(i,j);
-                uint8_t g1 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
-                uint8_t b1 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
-                
-                destination_pixel(i,j) = r1 | (g1 << 8) | (b1 << 16) | (a << 24);
-                
-                i = x + 1, j = y;
-                
-                uint8_t r2 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
-                uint8_t g2 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
-                uint8_t b2 = source_pixel(i,j);
-                
-                destination_pixel(i,j) = r2 | (g2 << 8) | (b2 << 16) | (a << 24);
-                
-                i = x, j = y;
-                
-                uint8_t r3 = (source_pixel(i,j-1) + source_pixel(i,j+1))/2;
-                uint8_t g3 = source_pixel(i,j);
-                uint8_t b3 = (source_pixel(i-1,j) + source_pixel(i+1,j))/2;
-                
-                destination_pixel(i,j) = r3 | (g3 << 8) | (b3 << 16) | (a << 24);
-                
-                i = x + 1, j = y + 1;
-                
-                uint8_t r4 = (source_pixel(i-1,j) + source_pixel(i+1,j))/2;
-                uint8_t g4 = source_pixel(i,j);
-                uint8_t b4 = (source_pixel(i,j-1) + source_pixel(i,j+1))/2;
-                
-                destination_pixel(i,j) = r4 | (g4 << 8) | (b4 << 16) | (a << 24);
+                if (self.mode == kCASImageDebayerGBRG){
+                    
+                    int i = x, j = y + 1;
+                    
+                    float r1 = source_pixel(i,j);
+                    float g1 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
+                    float b1 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
+                    
+                    destination_pixel(i,j) = make_rgba(r1,g1,b1,a);
+                    
+                    i = x + 1, j = y;
+                    
+                    float r2 = (source_pixel(i-1,j-1) + source_pixel(i+1,j-1) + source_pixel(i+1,j+1) + source_pixel(i-1,j+1))/4;
+                    float g2 = (source_pixel(i-1,j) + source_pixel(i,j-1) + source_pixel(i,j+1) + source_pixel(i+1,j))/4;
+                    float b2 = source_pixel(i,j);
+                    
+                    destination_pixel(i,j) = make_rgba(r2,g2,b2,a);
+                    
+                    i = x, j = y;
+                    
+                    float r3 = (source_pixel(i,j-1) + source_pixel(i,j+1))/2;
+                    float g3 = source_pixel(i,j);
+                    float b3 = (source_pixel(i-1,j) + source_pixel(i+1,j))/2;
+                    
+                    destination_pixel(i,j) = make_rgba(r3,g3,b3,a);
+                    
+                    i = x + 1, j = y + 1;
+                    
+                    float r4 = (source_pixel(i-1,j) + source_pixel(i+1,j))/2;
+                    float g4 = source_pixel(i,j);
+                    float b4 = (source_pixel(i,j-1) + source_pixel(i,j+1))/2;
+                    
+                    destination_pixel(i,j) = make_rgba(r4,g4,b4,a);
+                }
             }
         }
-    }
+    });
+    
+    NSLog(@"debayer: %fs",time);
     
     return CGBitmapContextCreateImage(context);
 }
