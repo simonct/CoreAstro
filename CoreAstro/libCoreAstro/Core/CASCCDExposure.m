1a6d645df8f4c584a46854b27256c8fc292e75ad
Simon Taylor <simon@makotechnology.com>
2013-01-19 19:03:31 +0000

    Remove -floatPixels logging noise, now only reset an exposures pixels and float pixels if it has an io backing store

--- CoreAstro/libCoreAstro/Core/CASCCDExposure.m
+++ CoreAstro/libCoreAstro/Core/CASCCDExposure.m
@@ -176,10 +176,12 @@
 {
     @synchronized(self){
         
-        NSData* pixels = self.pixels;
-        if (pixels && !_floatPixels){
+        if (!_floatPixels){
             
-            //const NSTimeInterval duration = CASTimeBlock(^{
+            NSData* pixels = self.pixels;
+            if (pixels){
+                
+                //const NSTimeInterval duration = CASTimeBlock(^{
                 
                 const NSInteger count = [pixels length] / sizeof(uint16_t);
                 float* fp = malloc(count * sizeof(float));
@@ -192,8 +194,9 @@
                     vDSP_vsdiv(fp,1,(float*)&max,fp,1,count);
                     _floatPixels = [NSData dataWithBytesNoCopy:fp length:(count * sizeof(float))];
                 }
-            //});
-            // NSLog(@"floatPixels: %fs",duration);
+                //});
+                // NSLog(@"floatPixels: %fs",duration);
+            }
         }
     }
     return _floatPixels;
@@ -363,9 +366,9 @@
     @synchronized(self){
         if (self.io){
             self.pixels = nil;
+            self.floatPixels = nil;
+            _readState = _meta ? kCASCCDExposureReadMeta : 0;
         }
-        self.floatPixels = nil;
-        _readState = _meta ? kCASCCDExposureReadMeta : 0;
     }
 }
 
