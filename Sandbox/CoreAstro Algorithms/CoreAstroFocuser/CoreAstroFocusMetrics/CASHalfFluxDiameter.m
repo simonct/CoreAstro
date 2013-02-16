9694fa540b9a96d4c224db81cd167279b490aa25
Wagner Truppel <trupwl@gmail.com>
2013-02-16 21:32:02 +0000

    CoreAstroAlgs: started work on CASHalfFluxDiameter. WIP.

--- Sandbox/CoreAstro Algorithms/CoreAstroFocuser/CoreAstroFocusMetrics/CASHalfFluxDiameter.m
+++ Sandbox/CoreAstro Algorithms/CoreAstroFocuser/CoreAstroFocusMetrics/CASHalfFluxDiameter.m
@@ -28,24 +28,69 @@
 
 #import "CASHalfFluxDiameter.h"
 
+
+@interface CASHalfFluxDiameter ()
+@property (readwrite, nonatomic) CGPoint brightnessCentroid;
+@end
+
+
 @implementation CASHalfFluxDiameter
 
 - (CGFloat) focusMetricForRegion: (CASRegion*) region
                  inExposureArray: (uint16_t*) values
                         ofLength: (NSUInteger) len
                          numRows: (NSUInteger) numRows
-                         numCols: (NSUInteger) numCols;
+                         numCols: (NSUInteger) numCols
+              brightnessCentroid: (CGPoint*) brightnessCentroidPtr;
 {
+    // XXX must not forget to set self.brightnessCentroid at the end!
+//    self.brightnessCentroid = XXX;
+//    *brightnessCentroidPtr = self.brightnessCentroid;
+
     return 0.0; // XXX
 }
 
 @end
 
-// XXX - argument params should be declared const ! (same for the other project)
+//extern NSString* const keyFocusMetric;
+//@interface CASFocusMetric: CASAlgorithm
+//@property (readonly, nonatomic, strong) CASCCDExposure* exposure;
+//@property (readonly, nonatomic, strong) CASRegion* region;
+//@property (readonly, nonatomic) NSUInteger numRows;
+//@property (readonly, nonatomic) NSUInteger numCols;
+//@property (readonly, nonatomic) NSUInteger numPixels;
+//@end
 
-// XXX here just for quick reference
+
+//typedef void(^CASAlgorithmCompletionBlock)(NSDictionary*);
+//@interface CASAlgorithm: NSObject
+//
+//@property (readonly, nonatomic, strong) NSDictionary* dataD;
+//@property (readonly, nonatomic) dispatch_queue_t completionQueue;
+//@property (readonly, nonatomic, strong) CASAlgorithmCompletionBlock completionBlock;
+//
+//// For subclass use only.
+//// Must be overridden as this is the meat of the algorithm.
+//// The default implementation returns nil.
+//- (NSDictionary*) resultsFromData: (NSDictionary*) dataD;
+//
+//// Utility method.
+//// For subclass use only and not to be overridden.
+//- (void) dispatchBlock: (CASAlgorithmCompletionBlock) block
+//               toQueue: (dispatch_queue_t) queue
+//                 async: (BOOL) async
+//          withArgument: (NSDictionary*) resultsD;
+//
+//@end
+
+
+//extern NSString* const keyRegion;
+//@interface CASRegion: NSObject
+//
 //@property (nonatomic) NSUInteger regionID;
 //@property (nonatomic) NSUInteger brightestPixelIndex; // index in the exposure array
 //@property (nonatomic) CASPoint brightestPixelCoords; // coordinates in the exposure image
 //@property (nonatomic) CASRect frame; // in image coordinates (origin at bottom-left corner)
 //@property (nonatomic, strong) NSSet* pixels; // set of NSNumber-boxed indices into the exposure array
+//
+//@end
