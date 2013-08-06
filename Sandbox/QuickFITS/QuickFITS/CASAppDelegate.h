1f73f844ecc2c1881d68f69c4d86224558f6f794
Simon Taylor <simonct@gmail.com>
2013-08-06 07:58:37 +0100

    Update cfitsio to cfitsio3350, add QuickFITS viewer app

---
+++ Sandbox/QuickFITS/QuickFITS/CASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CASAppDelegate.h
+//  QuickFITS
+//
+//  Created by Simon Taylor on 05/08/2013.
+//  Copyright (c) 2013 Simon Taylor. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CASAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Sandbox/QuickFITS/QuickFITS/CASAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  CASAppDelegate.m
+//  QuickFITS
+//
+//  Created by Simon Taylor on 05/08/2013.
+//  Copyright (c) 2013 Simon Taylor. All rights reserved.
+//
+
+#import "CASAppDelegate.h"
+
+@implementation CASAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
