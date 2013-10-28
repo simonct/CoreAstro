47d00b463a7a45e368908bc70fc9d71219008c6e
Simon Taylor <simon@coreastro.org>
2013-10-28 08:04:58 +0000

    Implement GeneratePreviewForURL

--- Sandbox/SX IO/QuickFITS/GeneratePreviewForURL.m
+++ Sandbox/SX IO/QuickFITS/GeneratePreviewForURL.m
@@ -1,6 +1,7 @@
-#include <CoreFoundation/CoreFoundation.h>
+#include <Foundation/Foundation.h>
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
+#include "CASFITSPreviewer.h"
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
@@ -13,8 +14,34 @@ void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
-    // To complete your generator please implement the function GeneratePreviewForURL in GeneratePreviewForURL.c
-    return noErr;
+    NSLog(@"QuickFITS: GeneratePreviewForURL: %@",url);
+    
+    int status = noErr; // todo; get from error object
+    
+    CASFITSPreviewer* previewer = [[CASFITSPreviewer alloc] init];
+    CGImageRef image = [previewer imageFromURL:(__bridge NSURL *)(url) error:nil];
+    if (!image){
+        NSLog(@"QuickFITS: GeneratePreviewForURL: failed to load image from url %@",url);
+    }
+    else{
+        
+        const CGSize previewSize = CGSizeMake(CGImageGetWidth(image), CGImageGetHeight(image));
+        
+        CGContextRef cgContext = QLPreviewRequestCreateContext(preview, *(CGSize *)&previewSize,true,NULL);
+        if (!cgContext){
+            NSLog(@"QuickFITS: GeneratePreviewForURL: QLPreviewRequestCreateContext returned nil");
+        }
+        else{
+            
+            CGContextDrawImage(cgContext, CGRectMake(0, 0, previewSize.width, previewSize.height), image);
+            QLPreviewRequestFlushContext(preview, cgContext);
+            CFRelease(cgContext);
+        }
+        
+        CGImageRelease(image);
+    }
+    
+    return status;
 }
 
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview)
