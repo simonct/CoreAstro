3b76ead494a23c85d4234d5022bb450507cadf0b
Simon Taylor <simon@coreastro.org>
2013-11-19 20:57:13 +0000

    Finally use the correct open/create fits routines to prevent filename template processing

---
+++ CoreAstro/libCoreAstro/Core/CASFITSUtilities.m
@@ -0,0 +1,24 @@
+//
+//  CASFITSUtilities.m
+//  CoreAstro
+//
+//  Created by Simon Taylor on 11/19/13.
+//  Copyright (c) 2013 Mako Technology Ltd. All rights reserved.
+//
+
+#import "CASFITSUtilities.h"
+
+int cas_fits_open_image(fitsfile **fptr,const char* path,int mode,int* status)
+{
+    *status = fits_open_diskfile(fptr,path,mode,status);
+    if (!*status){
+        
+        // from fits_open_image
+        int hdutype;
+        if (ffghdt(*fptr, &hdutype, status) <= 0) {
+            if (hdutype != IMAGE_HDU)
+                *status = NOT_IMAGE;
+        }
+    }
+    return *status;
+}
