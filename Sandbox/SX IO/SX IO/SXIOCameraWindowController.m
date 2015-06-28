0597fc43f6630d956acafa6b0d439c6e375bdb36
Simon Taylor <simonct@gmail.com>
2015-06-28 08:47:33 +0100

    Ensure Capture/Cancel button is re-enabled when the sequence ends

--- Sandbox/SX IO/SX IO/SXIOCameraWindowController.m
+++ Sandbox/SX IO/SX IO/SXIOCameraWindowController.m
@@ -320,7 +320,7 @@ static void* kvoContext;
 
 - (IBAction)cancelCapture:(id)sender
 {
-    self.captureButton.enabled = NO;
+    self.captureButton.enabled = NO; // the button is in Cancel mode, so disable it until the camera state is updated
     [self.cameraController cancelCapture];
 }
 
@@ -1717,7 +1717,7 @@ static void* kvoContext;
 
 - (void)endSequence
 {
-    [self cancelCapture:nil];
+    [self.cameraController cancelCapture]; // not calling -cancelCapture: any more as that disables the Cancel button
 }
 
 @end
