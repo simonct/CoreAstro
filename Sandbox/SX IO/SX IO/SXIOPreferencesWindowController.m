42b3e756ec2d158979e26d8df4db96aa543a33a0
Simon Taylor <simonct@gmail.com>
2015-08-10 10:24:48 +0100

    Fix formatter limits on latitude field, always update location when Update button pressed

--- Sandbox/SX IO/SX IO/SXIOPreferencesWindowController.m
+++ Sandbox/SX IO/SX IO/SXIOPreferencesWindowController.m
@@ -69,6 +69,9 @@
         self.locationManager = [[CLLocationManager alloc] init];
         self.locationManager.delegate = (id)self;
     }
+    if (self.locationManager.location){
+        [self handleLocationUpdate:self.locationManager.location];
+    }
     [self.locationManager startUpdatingLocation];
 }
 
