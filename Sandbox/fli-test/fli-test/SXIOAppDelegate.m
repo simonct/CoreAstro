e745a09db29b2a1b3b49902247120f283b52c5e6
Simon Taylor <simonct@gmail.com>
2015-12-28 15:49:32 +0000

    Add capture to fli-test

--- Sandbox/fli-test/fli-test/SXIOAppDelegate.m
+++ Sandbox/fli-test/fli-test/SXIOAppDelegate.m
@@ -8,11 +8,15 @@
 
 #import "SXIOAppDelegate.h"
 #import "FLISDK.h"
+#import "FLICCDDevice.h"
+#import "CASCCDExposureIO.h"
 
 @interface SXIOAppDelegate ()
 @property (weak) IBOutlet NSTableView *tableView;
 @property (weak) IBOutlet NSArrayController *devicesArrayController;
 @property (strong) FLISDK* sdk;
+@property NSInteger exposureMS;
+@property BOOL exposing;
 @end
 
 @implementation SXIOAppDelegate
@@ -25,10 +29,11 @@
     self.sdk.deviceAdded = ^(NSString* path,CASDevice* device){
         [weakSelf.devicesArrayController addObject:device];
         [device connect:^(NSError* error) {
-            NSLog(@"connect: %@",error);
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [NSApp presentError:error];
-            });
+            if (error){
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [NSApp presentError:error];
+                });
+            }
         }];
     };
     
@@ -38,6 +43,61 @@
 - (void)applicationWillTerminate:(NSNotification *)notification
 {
     [self.devicesArrayController.arrangedObjects makeObjectsPerformSelector:@selector(disconnect)];
+}
+
+- (IBAction)capture:(id)sender
+{
+    if (self.exposing){
+        NSLog(@"Busy");
+        return;
+    }
+    FLICCDDevice* ccd = self.devicesArrayController.selectedObjects.firstObject;
+    if (![ccd isKindOfClass:[FLICCDDevice class]]){
+        NSLog(@"No selected ccd");
+        return;
+    }
+    if (self.exposureMS < 1){
+        NSLog(@"Exposure time < 1");
+        return;
+    }
+
+    const CASExposeParams params = {
+        .bin = CASSizeMake(1, 1),
+        .origin = CASPointMake(0, 0),
+        .size = CASSizeMake(ccd.sensor.width, ccd.sensor.height),
+        .frame = CASSizeMake(ccd.sensor.width, ccd.sensor.height),
+        .bps = 16,
+        .ms = self.exposureMS
+    };
+    
+    self.exposing = YES;
+    
+    [ccd exposeWithParams:params type:kCASCCDExposureLightType block:^(NSError* error, CASCCDExposure *exposure) {
+        
+        self.exposing = NO;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (error){
+                [NSApp presentError:error];
+            }
+            else {
+                NSSavePanel* save = [NSSavePanel savePanel];
+                save.allowedFileTypes = @[@"fit"];
+                save.canCreateDirectories = YES;
+                [save beginWithCompletionHandler:^(NSInteger result) {
+                    
+                    if (result == NSFileHandlingPanelOKButton){
+                        NSError* error;
+                        CASCCDExposureIO* io = [CASCCDExposureIO exposureIOWithPath:save.URL.path];
+                        [io writeExposure:exposure writePixels:YES error:&error];
+                        if (error){
+                            [NSApp presentError:error];
+                        }
+                    }
+                }];
+            }
+        });
+    }];
 }
 
 @end
