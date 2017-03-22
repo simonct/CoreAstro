56d77720de2ee5b129758855674ec69c84ca0384
Simon <simonct@gmail.com>
2017-03-22 19:24:27 +0000

    Cache search results so that they can be used when the connection fails

--- CoreAstro/libCoreAstro/Core/CASObjectLookup.m
+++ CoreAstro/libCoreAstro/Core/CASObjectLookup.m
@@ -21,49 +21,59 @@
     
     NSURL* url = [NSURL URLWithString:[NSString stringWithFormat:@"http://simbad.u-strasbg.fr/simbad/sim-script?submit=submit+script&script=%@",script]];
     
+    NSURL* cacheDirectory = [[NSFileManager defaultManager] URLsForDirectory:NSCachesDirectory inDomains:NSUserDomainMask].firstObject;
+    NSURL* cacheLocation = [[cacheDirectory URLByAppendingPathComponent:[name lowercaseString]] URLByAppendingPathExtension:@"txt"];
+    
     NSURLRequest* request = [NSURLRequest requestWithURL:url];
     
     [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
         
+        NSString* responseString;
+        
         if (connectionError){
             NSLog(@"connectionError: %@",connectionError);
-            completion(NO,nil,0,0);
+            @synchronized ([self class]) {
+                responseString = [NSString stringWithContentsOfURL:cacheLocation encoding:NSUTF8StringEncoding error:nil];
+            }
         }
         else {
-            
-            BOOL foundIt = NO;
-            NSString* object;
-            double ra = 0, dec = 0;
-            
-            NSString* responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-            if (![responseString containsString:@"::error"]){
+            responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        }
+        
+        BOOL foundIt = NO;
+        NSString* object;
+        double ra = 0, dec = 0;
 
-                NSArray* responseLines = [responseString componentsSeparatedByString:@"\n"];
+        if (responseString && ![responseString containsString:@"::error"]){
+            
+            NSArray* responseLines = [responseString componentsSeparatedByString:@"\n"];
+            
+            for (NSString* line in [responseLines reverseObjectEnumerator]){
                 
-                for (NSString* line in [responseLines reverseObjectEnumerator]){
+                NSScanner* scanner = [NSScanner scannerWithString:line];
+                
+                if ([scanner scanUpToString:@":" intoString:&object]){
                     
-                    NSScanner* scanner = [NSScanner scannerWithString:line];
+                    NSMutableCharacterSet* cs = [NSMutableCharacterSet decimalDigitCharacterSet];
+                    [cs addCharactersInString:@"+-"];
                     
-                    if ([scanner scanUpToString:@":" intoString:&object]){
-                        
-                        NSMutableCharacterSet* cs = [NSMutableCharacterSet decimalDigitCharacterSet];
-                        [cs addCharactersInString:@"+-"];
-                        
+                    [scanner scanUpToCharactersFromSet:cs intoString:nil];
+                    if ([scanner scanDouble:&ra]){
                         [scanner scanUpToCharactersFromSet:cs intoString:nil];
-                        if ([scanner scanDouble:&ra]){
-                            [scanner scanUpToCharactersFromSet:cs intoString:nil];
-                            foundIt = [scanner scanDouble:&dec];
-                            if (foundIt){
-                                // NSLog(@"object: %@, ra: %f, dec: %f",object,ra,dec);
+                        foundIt = [scanner scanDouble:&dec];
+                        if (foundIt){
+                            @synchronized ([self class]) {
+                                [responseString writeToURL:cacheLocation atomically:YES encoding:NSUTF8StringEncoding error:nil];
                             }
                         }
-                        
-                        break;
                     }
-                };
-            }
-            completion(foundIt,object,ra,dec);
+                    
+                    break;
+                }
+            };
         }
+
+        completion(foundIt,object,ra,dec);
     }];
 }
 
