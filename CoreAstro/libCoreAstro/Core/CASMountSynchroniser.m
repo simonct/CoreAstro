b8166fe40484a554a1845ae5b54242e8ecc3fccc
Simon Taylor <simonct@gmail.com>
2018-05-27 16:18:48 +0100

    Reduce CASMountSlewControllerConvergence to 0.0025

--- CoreAstro/libCoreAstro/Core/CASMountSynchroniser.m
+++ CoreAstro/libCoreAstro/Core/CASMountSynchroniser.m
@@ -36,7 +36,7 @@
 {
     [[NSUserDefaults standardUserDefaults] registerDefaults:@{@"CASMountSlewControllerBinning":@(4),
                                                               @"CASMountSlewControllerDuration":@(5),
-                                                              @"CASMountSlewControllerConvergence":@(0.005), // relate to camera pixel size ?
+                                                              @"CASMountSlewControllerConvergence":@(0.0025), // relate to camera pixel size ?
                                                               @"CASMountSlewControllerSearchRadius":@(5),
                                                               @"CASMountSlewControllerAutoSyncSlewDuration":@(10)}];
 }
