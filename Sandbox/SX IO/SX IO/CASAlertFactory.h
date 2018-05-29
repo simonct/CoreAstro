82d31200a4a9eee35fc3563e408bdce149de0730
Simon Taylor <simonct@gmail.com>
2018-05-29 14:17:01 +0100

    Add CASAlertFactory files

---
+++ Sandbox/SX IO/SX IO/CASAlertFactory.h
@@ -0,0 +1,17 @@
+//
+//  CASAlertFactory.h
+//  CoreAstro
+//
+//  Created by Simon Taylor on 27/05/2018.
+//  Copyright © 2018 Mako Technology Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CASAlertFactory : NSObject
+
++ (NSAlert *_Nullable)alertWithMessageText:(nullable NSString *)message defaultButton:(nullable NSString *)defaultButton alternateButton:(nullable NSString *)alternateButton otherButton:(nullable NSString *)otherButton informativeTextWithFormat:(NSString *_Nullable)format, ... NS_FORMAT_FUNCTION(5,6);
+
++ (NSInteger)runModalAlertTitle:(NSString*)title message:(NSString*)message;
+
+@end
---
+++ Sandbox/SX IO/SX IO/CASAlertFactory.m
@@ -0,0 +1,42 @@
+//
+//  CASAlertFactory.m
+//  CoreAstro
+//
+//  Created by Simon Taylor on 27/05/2018.
+//  Copyright © 2018 Mako Technology Ltd. All rights reserved.
+//
+
+#import "CASAlertFactory.h"
+
+@implementation CASAlertFactory
+
++ (NSAlert *_Nullable)alertWithMessageText:(nullable NSString *)message defaultButton:(nullable NSString *)defaultButton alternateButton:(nullable NSString *)alternateButton otherButton:(nullable NSString *)otherButton informativeTextWithFormat:(NSString *_Nullable)format, ...
+{
+    NSAlert* alert = [[NSAlert alloc] init];
+    
+    alert.messageText = message;
+    alert.informativeText = format;
+    
+    if (defaultButton){
+        [alert addButtonWithTitle:defaultButton];
+    }
+    if (alternateButton){
+        [alert addButtonWithTitle:alternateButton];
+    }
+    if (otherButton){
+        [alert addButtonWithTitle:otherButton];
+    }
+    
+    return alert;
+}
+
++ (NSInteger)runModalAlertTitle:(NSString*)title message:(NSString*)message
+{
+    return [[CASAlertFactory alertWithMessageText:title
+                                    defaultButton:@"OK"
+                                  alternateButton:nil
+                                      otherButton:nil
+                        informativeTextWithFormat:message] runModal];
+}
+
+@end
